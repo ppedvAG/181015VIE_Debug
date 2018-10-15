278da6b138a27958c8600da24dba150a56d17037
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-15 11:05:10 +0200

    Breakpoints (Condition/Fenster/Export)

--- HalloDebugging/Program.cs
+++ HalloDebugging/Program.cs
@@ -10,10 +10,22 @@ namespace HalloDebugging
     {
         static void Main(string[] args)
         {
+            // Breakpoints mit Condition: Rechtsklick auf Breakpoint
+            // Breakpoint-Fenster: 
+            Console.WriteLine("Hallo Debugger");
 
+            HalloWelt();
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+
+        private static void HalloWelt()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hallo Welt");
+            }
         }
     }
 }
